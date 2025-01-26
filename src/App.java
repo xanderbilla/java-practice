eaa6e66a0beee4bf89c34c25acbf29e150be45e0
Vikas <xanderbilla@gmail.com>
2025-01-26 21:25:31 +0530

    Main Funciton

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
 import com.myclass.pep_classes.P5_LinkedList;
 import com.myclass.pep_classes.P6_AdvLinkdList;
+import com.myclass.pep_classes.P7_String;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // P5_LinkedList.test();
-        P6_AdvLinkdList.run();
+        // P6_AdvLinkdList.run();
+        P7_String.run();
     }
 }
